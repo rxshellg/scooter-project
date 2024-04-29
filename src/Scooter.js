80769ed6cb81882887d059ccf2c30713f1c422db
rxshellg <rashellguerrero123@gmail.com>
2024-04-28 20:53:04 -0400

    Completed Scooter class and tests

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,28 @@
+const User = require("./User")
+
 class Scooter {
-  // scooter code here
+  static nextSerial;
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+
+  rent(user) {
+    if (this.charge > 20 && this.isBroken == false) {
+      this.station = null;
+      this.user = user.username;
+    } else {
+      return `Scooter needs to charge or scooter needs repair`
+    }
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
 }
 
 module.exports = Scooter
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,21 +1,57 @@
 const Scooter = require('../src/Scooter')
+const User = require('../src/User')
 
 // typeof scooter === object
-describe('scooter object', () => {
+describe('Scooter object', () => {
   test('Scooter class should create Scooter instance', () => {
     const scooter = new Scooter()
     expect(scooter).toBeInstanceOf(Scooter)
   })
+
+  test('Properties are initialized correctly', () => {
+    const scooter = new Scooter("station 1")
+    expect(scooter.station).toBe("station 1")
+    expect(scooter.user).toBe(null)
+    expect(scooter.serial).toBe(undefined)
+    expect(scooter.charge).toBe(100)
+    expect(scooter.isBroken).toBe(false)
+  })
 })
 
 // Method tests
-describe('scooter methods', () => {
-  // tests here!
+describe('Rent method', () => {
+  beforeEach(() => {
+    scootie = new Scooter("station 1")
+    Rashell = new User("rxshellg", "password123", 21)
+  })
+  test('Accepts a user instance of the User class as an argument', () => {
+    scootie.rent(Rashell)
+    expect(scootie.user).toBe(Rashell.username)
+  })
 
-  // rent method
+  test('Successful checkout if scooter is charged above 20% and not broken', () => {
+    scootie.rent(Rashell)
+    expect(scootie.station).toBe(null)
+    expect(scootie.user).toBe("rxshellg")
+  })
 
+  test('Error if scooters battery is dead', () => {
+    scootie.charge = 19;
+    expect(scootie.rent(Rashell)).toBe("Scooter needs to charge or scooter needs repair")
+  })
+
+  test('Error if scooter is broken', () => {
+    scootie.isBroken = true;
+    expect(scootie.rent(Rashell)).toBe("Scooter needs to charge or scooter needs repair")
+  })
   // dock method
-
+  describe('Dock method', () => {
+    test('Can return the scooter to the station and clear out the user', () => {
+      scootie.dock("station 1")
+      expect(scootie.station).toBe("station 1")
+      expect(scootie.user).toBe(null)
+    })
+  })
   // requestRepair method
 
   // charge method
